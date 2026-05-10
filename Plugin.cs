971c1e5735ef448cff3d36db549f6ab8792e5a3c
SonPamungkas <nononon928@gmail.com>
2026-05-10 22:33:02 +0700

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -66,17 +66,7 @@ namespace NavalBuffetMod
 
                 if (matchedTarget != null)
                 {
-                    var traverse = Traverse.Create(__instance);
-                    bool origSingleUse = traverse.Field<bool>("singleUse").Value;
-                    float origRange = traverse.Field<float>("range").Value;
-                    
-                    Plugin.Log.LogInfo($"[Wiretap] Found TARGET Rearmer on GameObject: '{objName}' | original singleUse: {origSingleUse} | original range: {origRange}");
-
-                    var config = Plugin.Configs[matchedTarget];
-                    traverse.Field("singleUse").SetValue(!config.Replenishable.Value);
-                    traverse.Field("range").SetValue(config.Radius.Value);
-                    
-                    Plugin.Log.LogInfo($"[NavalBuffetMod] Success! {objName} is now set to singleUse: {traverse.Field<bool>("singleUse").Value}, range: {traverse.Field<float>("range").Value}");
+                    Plugin.Log.LogInfo($"[Wiretap] Found TARGET Rearmer on GameObject: '{objName}' | Initial config will be applied.");
                 }
             }
             catch (Exception ex)
@@ -86,10 +76,65 @@ namespace NavalBuffetMod
         }
     }
 
+    [HarmonyPatch(typeof(Rearmer), "RearmingCheck")]
+    public class Rearmer_RearmingCheck_Patch
+    {
+        static void Prefix(Rearmer __instance)
+        {
+            try
+            {
+                if (__instance == null || __instance.gameObject == null) return;
+                
+                string objName = __instance.gameObject.name;
+                string matchedTarget = null;
+                
+                foreach (var target in Plugin.Configs.Keys)
+                {
+                    if (objName.Contains(target))
+                    {
+                        matchedTarget = target;
+                        break;
+                    }
+                }
+
+                if (matchedTarget != null)
+                {
+                    var traverse = Traverse.Create(__instance);
+                    var config = Plugin.Configs[matchedTarget];
+                    
+                    traverse.Field("singleUse").SetValue(!config.Replenishable.Value);
+                    traverse.Field("range").SetValue(config.Radius.Value);
+                    
+                    // Update all physical trigger colliders in the object hierarchy
+                    foreach (var collider in __instance.GetComponentsInChildren<Collider>(true))
+                    {
+                        if (collider.isTrigger)
+                        {
+                            if (collider is SphereCollider sc)
+                            {
+                                sc.radius = config.Radius.Value;
+                            }
+                            else if (collider is CapsuleCollider cc)
+                            {
+                                cc.radius = config.Radius.Value;
+                            }
+                            else if (collider is BoxCollider bc)
+                            {
+                                // If it's a box collider trigger, scale the size to encompass the radius
+                                bc.size = new Vector3(config.Radius.Value * 2, config.Radius.Value * 2, config.Radius.Value * 2);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+    }
+
     public static class PluginInfo
     {
         public const string PLUGIN_GUID = "com.user.navalbuffetmod";
         public const string PLUGIN_NAME = "NavalBuffetMod";
-        public const string PLUGIN_VERSION = "1.1.0";
+        public const string PLUGIN_VERSION = "1.2.0";
     }
 }
